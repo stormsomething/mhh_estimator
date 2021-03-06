3e39ceb1b1b585b27143f17b18a41add1f9a52e5
qbuat <quentin.buat@cern.ch>
2021-03-06 11:50:36 -0500

    add default path

--- bbtautau/database.py
+++ bbtautau/database.py
@@ -1,6 +1,7 @@
+import os
 from .sample import sample
 
-PATH = '/Users/quentin/bbtautau_mhh/data/'
-
+DEFAULT_PATH = '/Users/quentin/bbtautau_mhh/data/'
+PATH = os.getenv("CXAOD_PATH", DEFAULT_PATH)
 dihiggs_01 = sample('HH_01', 'HH 01', 'red', 600023, PATH)
 dihiggs_10 = sample('HH_10', 'HH 10', 'blue', 600024, PATH)
